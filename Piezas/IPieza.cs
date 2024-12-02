d22cce28b2026f6ddda5e9b372839cae010de9cb
LilAguh <aguezeochoa@gmail.com>
2024-12-02 19:38:28 -0300

    interfaz de las piezas

---
+++ Piezas/IPieza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piezas
+{
+    public interface IPieza
+    {
+        bool EsMovimientoValido(int filaInicial, int colInicial, int filaDestino, int colDestino);
+        char ObtenerSimbolo();
+        bool EsPosicionValida(int fila, int columna, int[] posiciones);
+    }
+}
