29d35cb32818bb9b33bc669a71a4ce82686529ff
LilAguh <aguezeochoa@gmail.com>
2024-12-02 19:43:06 -0300

    se Programa la ejecucion de las 8 piezas en el tablero

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("que onda la bandana este es el ejercicio 2");
+﻿
+
+
+using Ejercicio2;
+using Piezas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tamañoTablero = 8;
+        Tablero tablero = new Tablero(tamañoTablero);
+
+        //Reina reina = new Reina();
+        IPieza reina = new Piezas.Reina();
+        IPieza torre = new Piezas.Torre();
+        IPieza alfil = new Piezas.Alfil();
+        int[] posiciones = new int[tamañoTablero];
+        for (int i = 0; i < tamañoTablero; i++) posiciones[i] = -1; // Inicializa las posiciones
+
+        int contadorSoluciones = 0;
+        tablero.Resolver(0, alfil, tamañoTablero, posiciones, ref contadorSoluciones);
+
+        Console.WriteLine($"Proceso terminado. Total de soluciones: {contadorSoluciones}");
+        // Pausa para evitar que la consola se cierre inmediatamente
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
