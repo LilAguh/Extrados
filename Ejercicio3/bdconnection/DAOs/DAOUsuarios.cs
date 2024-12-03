abf459b51308c4759981924fa9f9b992596697d8
LilAguh <aguezeochoa@gmail.com>
2024-12-03 07:27:13 -0300

    DAO de Usuarios

---
+++ Ejercicio3/bdconnection/DAOs/DAOUsuarios.cs
@@ -0,0 +1,35 @@
+﻿using Ejercicio3.bdconnection.Entidades;
+using MySqlConnector;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3.bdconnection.DAOs
+{
+    internal class DAOUsuarios
+    {
+        public List<Usuarios> ObtenerTodosLosUsuarios()
+        {
+            string query = "SELECT * FROM Usuarios;";
+            string connectionString = "Server=127.0.0.1;Database=ejercicio3DB;UId=aguh;Password=asd123;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var usuarios = connection.Query<Usuarios>(query).AsList();
+                return usuarios;
+            }
+        }
+        public Usuarios ObtenerUsuario(int id)
+        {
+            string query = "SELECT * FROM Usuarios where ID = @id;";
+            string connectionString = "Server=127.0.0.1;Database=ejercicio3DB;UId=aguh;Password=asd123;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var usuario = connection.QueryFirstOrDefault<Usuarios>(query, new {id = id});
+                return usuario;
+            }
+        }
+    }
+}
