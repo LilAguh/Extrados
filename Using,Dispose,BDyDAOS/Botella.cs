2f533798009af05c9d89114b2a2f921961487059
LilAguh <aguezeochoa@gmail.com>
2024-12-02 22:08:16 -0300

    Uso de using y dispose

---
+++ Using,Dispose,BDyDAOS/Botella.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Dispose_BDyDAOS
+{
+    public class Botella: IDisposable
+    {
+        public string estado{ get; set; }
+
+        public Botella()
+        {
+            estado = "Vacia";
+        }
+
+        // Nunca se debe eliminar el objeto botella "Llena"
+        public void LlenarBotella()
+        {
+            estado = "Llena";
+            Console.WriteLine($"Botella {estado}");
+        }
+
+        public void VaciarBotella()
+        {
+            estado = "Vacia";
+            Console.WriteLine($"Botella {estado}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Presiona cualquier tecla para eliminar la botella");
+            Console.ReadKey();
+            Console.WriteLine("*aca se deberia de ejecutar el dispose para gatillar el vaciador*");
+            this.VaciarBotella();
+            Console.WriteLine("*aca se elimina la botella una vez vacia*");
+        }
+    }
+}
---
+++ Using,Dispose,BDyDAOS/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Using_Dispose_BDyDAOS;
+
+Console.WriteLine("iniciando");
+
+using(Botella bot= new Botella())
+{
+    bot.LlenarBotella();
+}
