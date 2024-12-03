9ec49eb0746da24d7bc81ab5601d83c375b8745a
LilAguh <aguezeochoa@gmail.com>
2024-12-03 11:41:46 -0300

    Se inicializa el crud

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -5,12 +5,27 @@ using Ejercicio3.bdconnection.DAOs;
 using Ejercicio3.bdconnection.Entidades;
 
 DAOUsuarios bd = new DAOUsuarios();
-var usuarioByID = bd.ObtenerUsuario(13);
 
-Console.WriteLine(usuarioByID.Nombre);
+//Obtiene una persona por el id
+//var usuarioByID = bd.ObtenerUsuario(13);
+//Console.WriteLine(usuarioByID.Nombre);
 
+//Obtiene lista de todos los usuarios
 //List<Usuarios> usuarios = bd.ObtenerTodosLosUsuarios();
 //foreach (var usuario in usuarios)
 //{
-//    Console.WriteLine($"ID: {usuario.ID}, Nombre: {usuario.Nombre}, Edad: {usuario.Edad}");
+//Console.WriteLine($"ID: {usuario.ID}, Nombre: {usuario.Nombre}, Edad: {usuario.Edad}");
 //}
+
+//Crea un nuevo usuario
+//var crearUsuario = bd.CrearUsuario("Juan", 33);
+//Console.WriteLine($"ID: {crearUsuario.ID}, Nombre: {crearUsuario.Nombre}, Edad: {crearUsuario.Edad}");
+
+//Actualiza un usuario
+//var actualizarUsuario = bd.ActualizarUsuario(26,"Adrian");
+//Console.WriteLine($"ID: {actualizarUsuario.ID}, Nombre: {actualizarUsuario.Nombre}, Edad: {actualizarUsuario.Edad}");
+
+//Elimina logicamente el usuario del sistema
+var eliminarUsuario = bd.DesactivarUsuario(22);
+if (eliminarUsuario)
+    Console.WriteLine("El usuario fue eliminado");
