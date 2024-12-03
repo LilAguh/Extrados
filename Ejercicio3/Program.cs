6d65a9c6bfb058a1dfffc850fa129a5f473c3749
LilAguh <aguezeochoa@gmail.com>
2024-12-03 07:26:06 -0300

    Obtener lista de usuarios

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Ejercicio3.bdconnection.DAOs;
+using Ejercicio3.bdconnection.Entidades;
+
+DAOUsuarios bd = new DAOUsuarios();
+var usuarioByID = bd.ObtenerUsuario(13);
+
+Console.WriteLine(usuarioByID.Nombre);
+
+//List<Usuarios> usuarios = bd.ObtenerTodosLosUsuarios();
+//foreach (var usuario in usuarios)
+//{
+//    Console.WriteLine($"ID: {usuario.ID}, Nombre: {usuario.Nombre}, Edad: {usuario.Edad}");
+//}
