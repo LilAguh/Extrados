af92b6613742837ac91067e6d1c123ecb14ad638
LilAguh <aguezeochoa@gmail.com>
2024-12-03 07:28:40 -0300

    Entidades de Usuarios

---
+++ Ejercicio3/bdconnection/Entidades/Usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3.bdconnection.Entidades
+{
+    internal class Usuarios
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
