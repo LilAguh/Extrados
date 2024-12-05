410f444653a659fc45b679fd750a17984195ecf0
LilAguh <aguezeochoa@gmail.com>
2024-12-05 12:41:29 -0300

    Exception perzonalizo propio del ejercicio

---
+++ Ejercicio5Excepciones/SinSolucionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Excepciones
+{
+    internal class SinSolucionException : Exception
+    {
+        public SinSolucionException(string mensaje) : base(mensaje) { }
+    }
+}
