2ff0fb9a4c7ba09e9395dea297a76f5f73618bd7
LilAguh <aguezeochoa@gmail.com>
2024-12-05 11:47:32 -0300

    Excepciones y guardas

--- Excepciones,GuardasYSingleton/Program.cs
+++ Excepciones,GuardasYSingleton/Program.cs
@@ -1,40 +1,75 @@
 ﻿
 
-Console.WriteLine("1");
-Console.WriteLine("2");
-Console.WriteLine("3");
+using Excepciones_GuardasYSingleton;
 
-try
-{
-    Contar();
-}
-catch(Exception e)
-{
-    Console.WriteLine("--------------");
-    Console.WriteLine("Mensaje: ");
-    Console.WriteLine(e.Message);
-    Console.WriteLine("StackTrace: ");
-    Console.WriteLine(e.StackTrace);
-    Console.WriteLine("--------------");
-}
-Console.WriteLine("12");
-Console.WriteLine("13");
-Console.WriteLine("14");
+//Console.WriteLine("1");
+//Console.WriteLine("2");
+//Console.WriteLine("3");
 
-void Contar()
+//try
+//{
+    //Contar();
+//}
+//catch(MiExcepcion e)
+//{
+//    Console.WriteLine("Se disparo MiExcepcion");
+//}
+//catch(Exception e)
+//{
+//    Console.WriteLine("Se disparo exception");
+//}
+//Console.WriteLine("12");
+//Console.WriteLine("13");
+//Console.WriteLine("14");
+
+//void Contar()
+//{
+//    Console.WriteLine("4");
+//    Console.WriteLine("5");
+//    Contar2();
+//    Console.WriteLine("10");
+//    Console.WriteLine("11");
+//}
+
+//void Contar2()
+//{
+//    Console.WriteLine("6");
+//    Console.WriteLine("7");
+//    throw new MiExcepcion();
+//    Console.WriteLine("8");
+//    Console.WriteLine("9");
+//}
+
+
+
+//Codigo normal
+void VenderAlcohol(int edad, bool veda)
 {
-    Console.WriteLine("4");
-    Console.WriteLine("5");
-    Contar2();
-    Console.WriteLine("10");
-    Console.WriteLine("11");
+    if(edad >= 18 & !veda)
+    {
+        Console.WriteLine("Se le puede vender alcohol.");
+    }
+    else
+    {
+        Console.WriteLine("No vender alcohol.");
+    }
 }
 
-void Contar2()
+VenderAlcohol(21, true);
+
+Console.WriteLine("----------------------------------------");
+
+
+// Codico con Guarda
+//se utiliza para evitar la anidación de IF. cada nuevo nivel de IF que se tenga en el codigo
+//agrega complejidad para leerlo y debe ser evitado de ser posible.
+void VenderAlcoholGuarda(int edad, bool veda)
 {
-    Console.WriteLine("6");
-    Console.WriteLine("7");
-    throw new Exception("Error al contar: no puede contar mas de 7");
-    Console.WriteLine("8");
-    Console.WriteLine("9");
+    //  en vez de pensar en el codigo de forma “si esto se cumple, corre el codigo”,
+    //  pensar en forma de “si esto no se cumple, tira error”
+    if (edad < 18 || veda)
+        throw new Exception("No vender alcohol.");
+    Console.WriteLine("Se le puede vender alcohol.");
 }
+
+VenderAlcoholGuarda(21, true);
