4f0ed52451165580a02901176c4a0ae8775dc7d6
LilAguh <aguezeochoa@gmail.com>
2024-12-09 07:10:29 -0300

    Exception de stock negativo

---
+++ Ejercicio6GuardasYSingleton/StockNegativoException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6GuardasYSingleton
+{
+    public class StockNegativoException : Exception
+    {
+        public StockNegativoException(string mensaje) : base(mensaje) { }
+    }
+}
