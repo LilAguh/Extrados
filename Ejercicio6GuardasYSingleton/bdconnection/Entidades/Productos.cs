e11d470be3aae67a587e41b0c91bbbd8100fa30d
LilAguh <aguezeochoa@gmail.com>
2024-12-09 07:24:06 -0300

    Entidades de Productos

--- Ejercicio6GuardasYSingleton/bdconnection/Entidades/Productos.cs
+++ Ejercicio6GuardasYSingleton/bdconnection/Entidades/Productos.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Ejercicio6GuardasYSingleton.bdconnection.Entidades
 {
-    internal class Productos
+    public class Productos
     {
+        public int ID { get; set; }
+        public string Producto { get; set; }
+        public int Precio { get; set; }
+        public int Stock { get; set; }
+        public bool AptoMenores { get; set; }
+        public bool Alcohol { get; set; }
     }
 }
